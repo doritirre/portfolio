b8b4aa2034d99513f619d3667028e55dc10ee65c
doritirre <doritirre@gmail.com>
2020-06-02 20:46:31 -0300

    Últimos acertos da inserção dos vídeos

--- modulo02/scripts.js
+++ modulo02/scripts.js
@@ -3,10 +3,13 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const videoID = card.getAttribute('id')
         modal_overlay.classList.add('active')
+        modal_overlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoID}`
     })
 }
 
 document.querySelector('.close_modal').addEventListener("click", function() {
-    modal_overlay.classList.remove('active')
+    modal_overlay.classList.remove('active')   
+    modal_overlay.querySelector('iframe').src = ""     
 } )
