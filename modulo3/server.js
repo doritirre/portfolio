922b634846211066def3ff415acc9f5f68ebd13c
doritirre <doritirre@gmail.com>
2020-06-03 13:20:38 -0300

    O uso do Nunjucks para fazer back-end com os dados, além do uso de templates

--- modulo3/server.js
+++ modulo3/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get("/", (request, response) => {
-    return response.send('Hello Word')
+    return response.render("about")
+})
+
+server.get("/portfolio", (request, response) => {
+    return response.render("portfolio")
 })
 
 server.listen(5000, () => console.log("Server is Running"))
