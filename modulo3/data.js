b0889d750abe121cca1892bc5d365015d03499c7
doritirre <doritirre@gmail.com>
2020-06-08 12:42:08 -0300

    Termino da pasta Usando dados dinamicos

---
+++ modulo3/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+   {
+    id: "Vw9BG9fauIE",
+    title: "É hora de estudar o Deno?",
+    duration: "57 min",
+    price: "Free"
+   },
+   {
+    id: "U0wQjppkFdY",
+    title: "Ilda Neta - Mobile Developer Banco ABC Brasil | Alumni #02",
+    duration: "1 hora",
+    price: "Free"
+   },
+   {
+    id: "NLcXijzqP9M",
+    title: "Começar pelo TypeScript ou JavaScript? Python, Deno, React Native ou Flutter?",
+    duration: "7 min",
+    price: "Free"
+   },
+   {
+    id: "6wn8hpUcEcM",
+    title: "Drag and Drop Raiz com a API do HTML, CSS e JavaScript | Masterclass #16",
+    duration: "55 min",
+    price: "Free"
+   },
+   {
+    id: "Vw9BG9fauIE",
+    title: "É hora de estudar o Deno?",
+    duration: "57 min",
+    price: "Free"
+   },
+   {
+    id: "Vw9BG9fauIE",
+    title: "É hora de estudar o Deno?",
+    duration: "57 min",
+    price: "Free"
+   }
+]
--- modulo3/server.js
+++ modulo3/server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks');
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", (request, response) => {
-    return response.render('about')
+    const about = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/6643122?s=460&v=4",
+        name: "Maik Brito",
+        role: "Instrututor - Rocketseat",
+        description: 'Programador full-stak, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseta.com.br" target="_blank"> Rocketseat</a>',
+        links: [
+            {name: "Github", url: "https://github.com/maykbrito"},
+            {name: "Twitter", url: "https://github.com/maykbrito"},
+            {name: "Linkedin", url: "https://github.com/maykbrito"}
+        ]
+    }
+
+    return response.render('about', {about: about})
 })
 
 server.get("/portfolio", (request, response) => {
-    return response.render('portfolio')
+    return response.render('portfolio', { items: videos })
 })
 
 server.listen(5000, () => console.log("Server is Running"))
