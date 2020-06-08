393b4e45fb1fb16a37f70775d20ad076a8c90b42
doritirre <doritirre@gmail.com>
2020-06-08 13:33:35 -0300

    Termino da pasta Videos em destaque

--- modulo3/data.js
+++ modulo3/data.js
@@ -3,36 +3,42 @@ module.exports = [
     id: "Vw9BG9fauIE",
     title: "É hora de estudar o Deno?",
     duration: "57 min",
-    price: "Free"
+    price: "Free",
+    featured: true
    },
    {
     id: "U0wQjppkFdY",
     title: "Ilda Neta - Mobile Developer Banco ABC Brasil | Alumni #02",
     duration: "1 hora",
-    price: "Free"
+    price: "Free",
+    featured: false
    },
    {
     id: "NLcXijzqP9M",
     title: "Começar pelo TypeScript ou JavaScript? Python, Deno, React Native ou Flutter?",
     duration: "7 min",
-    price: "Free"
+    price: "Free",
+    featured: true
    },
    {
     id: "6wn8hpUcEcM",
     title: "Drag and Drop Raiz com a API do HTML, CSS e JavaScript | Masterclass #16",
     duration: "55 min",
-    price: "Free"
+    price: "Free",
+    featured: false
    },
    {
     id: "Vw9BG9fauIE",
     title: "É hora de estudar o Deno?",
     duration: "57 min",
-    price: "Free"
+    price: "Free",
+    featured: false
    },
    {
     id: "Vw9BG9fauIE",
     title: "É hora de estudar o Deno?",
     duration: "57 min",
-    price: "Free"
+    price: "Free",
+    featured: true
    }
 ]
